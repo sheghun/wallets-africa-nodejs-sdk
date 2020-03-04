aede5bbd0815b4d1158852f03f9822549e0bbe85
Redeemer40 <victorjonah199@gmail.com>
2020-03-04 07:10:16 +0100

    Added transactionOptions to test

--- tests/self.spec.ts
+++ tests/self.spec.ts
@@ -6,35 +6,45 @@ import {assert, expect} from 'chai';
  */
 
 // The key is supplied from package.json under the scripts command
-const wallet = new WalletAfrica({
+const walletAfrica = new WalletAfrica({
     secretKey: process.env.SECRET_KEY as string,
     publicKey: process.env.PUBLIC_KEY as string,
     sandbox: true,
 });
 
 describe('Self resource tests', function() {
-    it('Check balance', async function() {
-        const res = await wallet.self.checkBalance();
+    it('Checks balance', async function() {
+        const res = await walletAfrica.self.checkBalance('NGN');
         assert.strictEqual(res.status, 200);
         assert.containsAllDeepKeys(res.data, {
             Response: {
                 ResponseCode: '200',
+                Message: 'Balance Retrieved successfully',
             },
         });
     });
 
     it('Transactions', async function() {
-        const res = await wallet.self.transactions();
+        const res = await walletAfrica.self.transactions({
+            currency: 'NGN',
+            transactionPin: '1111',
+            transactionType: 1,
+            dateFrom: '2020-01-15',
+            dateTo: '2020-02-03',
+            take: 10,
+            skip: 0,
+        });
         assert.strictEqual(res.status, 200);
         assert.containsAllDeepKeys(res.data, {
             Response: {
                 ResponseCode: '200',
+                Message: 'Transaction Retrieved successfully',
             },
         });
     });
 
     it('Verify BVN', async function() {
-        const res = await wallet.self.verifyBvn({
+        const res = await walletAfrica.self.verifyBvn({
             bvn: process.env.BVN as string,
             dateOfBirth: process.env.BVN_DATE as string,
         });
@@ -45,7 +55,7 @@ describe('Self resource tests', function() {
     });
 
     it('Get Wallets', async function() {
-        const res = await wallet.self.getWallets();
+        const res = await walletAfrica.self.getWallets();
         assert.strictEqual(res.status, 200);
         assert.containsAllDeepKeys(res.data, {
             Response: {
